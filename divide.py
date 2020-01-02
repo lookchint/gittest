695af0b60fb5d08fe84c8939496d0b86cc48db0d
Intern <mos.krittaboon@gmail.com>
2020-01-02 16:58:32 +0700

    Sawaddeekub

--- divide.py
+++ divide.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 p1 = input("Enter Num1 : ")
 p2 = input("Enter Num2 : ")
 num1 = float(p1)
 num2 = float(p2)
 
 print("Your value is : ", num1 / num2)
-=======
-pound love cola
->>>>>>> 8e98578631c9bd539ddd4ac53fe692b1e2b7d6c7
+
