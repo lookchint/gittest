4ff733e76f80be64c83211cc9a9e44320de5eae5
Intern <mos.krittaboon@gmail.com>
2020-01-03 14:47:19 +0700

    EWW

--- divide.py
+++ divide.py
@@ -1,14 +1,7 @@
-import numpy
-
 p1 = input("Enter Num1 : ")
 p2 = input("Enter Num2 : ")
+
 num1 = float(p1)
 num2 = float(p2)
 
-
-def divide(num1, num2):
-    result = num1 / num2
-    print("Your value is : ", result)
-
-
-divide(num1, num2)
+print("Your value is : ", num1 / num2)
