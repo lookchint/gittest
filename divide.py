bfdaeb8bc8e9a9eeadec807ee382d53035ebd0af
hutgun11@gmail.com <34814323+hutgun11@users.noreply.github.com>
2020-01-03 15:33:59 +0700

    autoreload

--- divide.py
+++ divide.py
@@ -1,10 +1,14 @@
-p1 = input("Enter Num1 : ")
-p2 = input("Enter Num2 : ")
-num1 = float(p1)
-num2 = float(p2)
-def divide(num1,num2):
-    result = num1 / num2
-    print("Your value is : ", result)
+# p1 = input("Enter Num1 : ")
+# p2 = input("Enter Num2 : ")
+# num1 = float(p1)
+# num2 = float(p2)
 
-divide(num1,num2)
+
+def divides(num1, num2):
+    result = num1 * num2 /50000
+    return result
+
+
+def result(x):
+    return x
 
