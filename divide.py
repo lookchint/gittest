94d51b57ad4587d98e61f56a43ff67641776d06a
hutgun11@gmail.com <34814323+hutgun11@users.noreply.github.com>
2020-01-03 15:53:01 +0700

    edit divide

--- divide.py
+++ divide.py
@@ -1,30 +1,7 @@
-<<<<<<< HEAD
-# p1 = input("Enter Num1 : ")
-# p2 = input("Enter Num2 : ")
-# num1 = float(p1)
-# num2 = float(p2)
-
-
 def divides(num1, num2):
-    result = num1 * num2 /50000
+    result = num1 * num2 / 50000
     return result
 
 
 def result(x):
     return x
-=======
-import numpy
-
-p1 = input("Enter Num1 : ")
-p2 = input("Enter Num2 : ")
-num1 = float(p1)
-num2 = float(p2)
-
-
-def divide(num1, num2):
-    result = num1 / num2
-    print("Your value is : ", result)
-
->>>>>>> c86ad916df98682e8bb8dfcbacce5cba9fd076a6
-
-divide(num1, num2)
